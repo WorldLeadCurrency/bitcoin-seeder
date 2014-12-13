ab778912535f4102d954a275b03f90d13c7f32b0
Rikski <hendrik-richter@freenet.de>
2014-12-13 15:31:50 +0100

    update to WLC #1

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -296,7 +296,7 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
 
 /*
 int main(void) {
-  CService ip("bitcoin.sipa.be", 8333, true);
+  CService ip("wlchost.no-ip.org", 55889, true);
   vector<CAddress> vAddr;
   vAddr.clear();
   int ban = 0;
