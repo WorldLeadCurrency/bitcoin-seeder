90b341f6605f3a57cdf4b4b465d6c4c955171295
Rikski <hendrik-richter@freenet.de>
2014-12-13 15:37:35 +0100

    update to WLC #4

--- main.cpp
+++ main.cpp
@@ -339,13 +339,13 @@ extern "C" void* ThreadStats(void*) {
   } while(1);
 }
 
-static const string mainnet_seeds[] = {"dnsseed.bluematt.me", "bitseed.xf2.org", "dnsseed.bitcoin.dashjr.org", "seed.bitcoin.sipa.be", ""};
-static const string testnet_seeds[] = {"testnet-seed.bitcoin.petertodd.org", "static-testnet-seed.bitcoin.petertodd.org", ""};
+static const string mainnet_seeds[] = {"wlcvpshost.no-ip.org", "wlchost.no-ip.org", ""};
+static const string testnet_seeds[] = {"wlchost.no-ip.org", ""};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
   if (!fTestNet){
-    db.Add(CService("kjy2eqzk4zwi5zd3.onion", 8333), true);
+    db.Add(CService("kjy2eqzk4zwi5zd3.onion", 55889), true);
   }
   do {
     for (int i=0; seeds[i] != ""; i++) {
